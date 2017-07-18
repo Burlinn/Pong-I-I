5a6fbf245676a16aca8c9763097a961ebd9ab757
Burlinn <nicholasaburlingame@gmail.com>
2017-07-17 20:23:31 -0400

    Pong I I Basic
    The Initial Check-In. Just a simple working version of Pong

---
+++ Assets/Pong Tutorial/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public int speed = 10;
+    public GameObject ball;
+
+    // Update is called once per frame
+    void Update() {
+        if (ball != null) { 
+            //Make our enemy track the ball
+            if (ball.transform.position.y > transform.position.y)
+            {
+                transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+            }
+            if (ball.transform.position.y < transform.position.y)
+            {
+                transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
+            }
+
+            //Check top bounds
+            if (transform.position.y > 6.75)
+            {
+                Vector3 holdAtTop = transform.position;
+                holdAtTop.y = 6.75f;
+                transform.position = holdAtTop;
+            }
+            if (transform.position.y < -6.75)
+            {
+                Vector3 holdAtBottom = transform.position;
+                holdAtBottom.y = -6.75f;
+                transform.position = holdAtBottom;
+            }
+        }   
+    }
+}
